eb29d034f1a4a6edfcaf4cdb9145adb60aab11b3
atmabdullaham <atmabdullaham@gmail.com>
2024-11-12 14:12:31 +0600

    first commit

---
+++ questions.js
@@ -0,0 +1,9 @@
+// 1. You are analyzing a list of flags. Skip over any "red flag" and print the rest. Use continue to skip these items.
+
+// 2. Write a for loop to print numbers from 1 to 10, but skip numbers divisible by 3 using continue.
+
+// 3. Given an array of names, skip over any empty strings using continue and print the rest.
+
+// 4. You are analyzing test scores. Skip any score below 60 and print only the passing scores using continue.
+
+// 5. You have a list of products. Skip over any product marked as "on sale" and print the rest using continue.
